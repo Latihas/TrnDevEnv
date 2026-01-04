cdadad182626b9ae5de5ae132022b2edae2d8c6e
Latihas <lyjp2002@163.com>
2026-01-04 15:49:15 +0800

    Initial commit

---
+++ Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Triggernometry.PScript;
+using static Triggernometry.PScript.ScriptUtils;
+
+public class Test : IScriptBase {
+    public override uint[] TerritoryIds() => [348];
+    public override List<TargetIcon> TargetIconList => [];
+    public override List<StartsCasting> StartsCastingList => [];
+    public override List<StatusAdd> StatusAddList => [
+        new(() => DrawShape(new IGCircle(Me_Position_D, 5, 5000)), 0x171, TargetId: Me_HexID_D),
+        new(() => DrawShape(new IGCone(Me_Position_D, 20, Me_Rotation_D, Math.PI, 5000)), 0x171, TargetId: Me_HexID_D),
+        new((_, s, _, _) => DrawShape(new IGRect(Me_Position_D, GetGameObjectById_Position(s), 5000, 10)), 0x171, TargetId: Me_HexID_D)
+    ];
+}
---
+++ TestPresets.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Forms;
+using Advanced_Combat_Tracker;
+using Triggernometry.Core;
+
+public class TestPresets : IActPluginV1 {
+    public void InitPlugin(TabPage pluginScreenSpace, Label pluginStatusText) {
+        RealPlugin.Instance.InvokeNamedCallback(
+            "preset", "{\n  \"Name\": \"Slot5\",\n  \"MapID\": 676,\n  \"A\": {\n    \"X\": -171,\n    \"Y\": 0,\n    \"Z\": 465,\n    \"Active\": true\n  },\n  \"C\": {\n    \"X\": -171,\n    \"Y\": 0,\n    \"Z\": 471,\n    \"Active\": true\n  }\n}");
+    }
+
+    public void DeInitPlugin() {
+        throw new NotImplementedException();
+    }
+}
